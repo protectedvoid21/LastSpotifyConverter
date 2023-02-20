5d5ac0795ac815ed019e987bf5c87a2a8fe4d72a
protectedvoid21 <nieznanyzero@interia.pl>
2023-02-20 11:47:47 +0100

    add Azure Key Vault

--- LastSpConverter/Program.cs
+++ LastSpConverter/Program.cs
@@ -1,12 +1,31 @@
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
 using Services.LastFm;
 using Services.Spotify;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddRazorPages();
 
 builder.Services.AddTransient<ILastFmService, LastFmService>();
 builder.Services.AddSingleton<ISpotifyService, SpotifyService>();
+
+string keyVaultUrl = builder.Configuration["KeyVault:Url"];
+
+var secretClient = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+if(builder.Environment.IsProduction()) {
+    builder.Configuration.AddAzureKeyVault(secretClient, new AzureKeyVaultConfigurationOptions());
+}
+
+var spotifySecret = await secretClient.GetSecretAsync("SpotifySecret");
+var spotifyClient = await secretClient.GetSecretAsync("SpotifyClient");
+var lastFmKey = await secretClient.GetSecretAsync("LastFmKey");
+
+builder.Configuration["spotify-secret"] = spotifySecret.Value.Value;
+builder.Configuration["spotify-client"] = spotifyClient.Value.Value;
+builder.Configuration["lastfm-key"] = lastFmKey.Value.Value;
 
 builder.Services.AddHttpClient<ILastFmService, LastFmService>("LastFm", client => {
     client.BaseAddress = new Uri("http://ws.audioscrobbler.com/2.0/");
